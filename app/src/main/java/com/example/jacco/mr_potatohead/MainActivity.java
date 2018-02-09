340a72373b8cdddf77ac4d6bbc88722ee5732312
JaccovanWijk <jaccov.wijk@yahoo.com>
2018-02-09 23:54:04 +0100

    first commit

--- app/src/main/java/com/example/jacco/mr_potatohead/MainActivity.java
+++ app/src/main/java/com/example/jacco/mr_potatohead/MainActivity.java
@@ -2,15 +2,41 @@ package com.example.jacco.mr_potatohead;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void checkClicked(View v) {
+        Log.d("potato", "checkClicked: ");
+
+        CheckBox checkbox = (CheckBox) v;
+        String name = checkbox.getText().toString();
+        boolean checked = checkbox.isChecked();
+
+        ImageView Arms = (ImageView) findViewById(R.id.Arms);
+        ImageView Shoes = (ImageView) findViewById(R.id.Shoes);
+        ImageView Eyes = (ImageView) findViewById(R.id.Eyes);
+        ImageView Eyebrow = (ImageView) findViewById(R.id.Eyebrow);
+        ImageView Glasses = (ImageView) findViewById(R.id.Glasses);
+        ImageView Hat = (ImageView) findViewById(R.id.Hat);
+        ImageView Nose = (ImageView) findViewById(R.id.Nose);
+        ImageView Moustache = (ImageView) findViewById(R.id.Moustache);
+        ImageView Mouth = (ImageView) findViewById(R.id.Mouth);
+        ImageView Ears = (ImageView) findViewById(R.id.Ears);
+
+        ImageView[] Photos = {Arms,Shoes,Eyes,Eyebrow,Glasses,Hat,Nose,Moustache,Mouth,Ears};
+
+        //Check if checkbox is checked, if so make visible
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void checkClicked(View v) {
-        Log.d(tag:"potato", msg:"checkClicked");
-    }
+
 }
