7ff1db64bd6489d02ecd734d5f31cada826e71c5
JaccovanWijk <jaccov.wijk@yahoo.com>
2018-02-09 12:51:42 +0100

    first commit

---
+++ app/src/main/java/com/example/jacco/mr_potatohead/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jacco.mr_potatohead;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void checkClicked(View v) {
+        Log.d(tag:"potato", msg:"checkClicked");
+    }
+}
